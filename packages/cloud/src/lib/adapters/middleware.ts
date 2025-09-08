6223a929acbef0f2bd185d426dad7c35cf2293b5
jrhizor <me@jaredrhizor.com>
2025-09-08 13:01:01 -0700

    clean up redundant middleware rules

--- packages/cloud/src/lib/adapters/middleware.ts
+++ packages/cloud/src/lib/adapters/middleware.ts
@@ -1,16 +1,3 @@
-import {
-  type ClerkMiddlewareAuth,
-  clerkMiddleware,
-  createRouteMatcher,
-} from "@clerk/nextjs/server";
-import type { NextRequest } from "next/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/icon(.*)"]);
-
-export default clerkMiddleware(
-  async (auth: ClerkMiddlewareAuth, req: NextRequest) => {
-    if (!isPublicRoute(req)) {
-      await auth.protect();
-    }
-  }
-);
+export default clerkMiddleware();
